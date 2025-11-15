e7de7e8f96e821a344df65c3bee92abf8bc3ef07
Sachin Arkasali <sachinarkasali762@gmail.com>
2025-11-15 13:40:13 +0530

    Initial Project setup

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Sachin Arkasali on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTracker/CryptoTrackerTests/CryptoTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoTrackerTests.swift
+//  CryptoTrackerTests
+//
+//  Created by Sachin Arkasali on 15/11/25.
+//
+
+import Testing
+
+struct CryptoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
